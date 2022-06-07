dbb6708945aa83fae65d0a96ba071e6e5944c4d9
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-07 15:11:53 +0530

    Feat(navbar) Navbar completed

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -4,6 +4,8 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../../images/logo.png";
 
+// navbar completed
+
 const NavbarItem = ({ title, classProps }) => {
   return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
 };
