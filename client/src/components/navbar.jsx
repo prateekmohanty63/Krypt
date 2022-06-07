3949c974abbc166bd2b3e4503990e29e5a7cd9a4
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-07 12:23:28 +0530

    Feat(Navbar) Started navbar and added images folder

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from '../../images/logo.png'
+
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial"></ul>
+    </nav>
   )
 }
 
